23b7479e3da1fc4e91131c2c22610684c123c0dd
Sebastian Pütz <spuetz@uos.de>
2017-07-03 17:50:27 +0200

    added last comments for abstract controller execution

--- move_base_flex/include/move_base_flex/abstract_server/abstract_controller_execution.h
+++ move_base_flex/include/move_base_flex/abstract_server/abstract_controller_execution.h
@@ -56,10 +56,11 @@ namespace move_base_flex
 {
 /**
  * @brief The AbstractPlannerExecution class loads and binds the local planner plugin. It contains a thread running
- *   the plugin in a cycle to move the robot. An internal state is saved and will be pulled by server, which controls
- *   the local planner execution. Due to a state change it wakes up all threads connected to the ondition variable.
+ *        the plugin in a cycle to move the robot. An internal state is saved and will be pulled by server, which
+ *        controls the local planner execution. Due to a state change it wakes up all threads connected to the ondition
+ *        variable.
  * @tparam LOCAL_PLANNER_BASE The base class derived from the AbstractLocalPlanner class,
- *   which determines the plugin class
+ *         which determines the plugin class
  */
 
 template<typename LOCAL_PLANNER_BASE>
@@ -163,92 +164,143 @@ template<typename LOCAL_PLANNER_BASE>
     void initialize();
 
     /**
-     * Is called by the server thread to reconfigure the controller execution.
+     * @brief Is called by the server thread to reconfigure the controller execution, if a user uses dynamic reconfigure
+     *        to reconfigure the current state.
      * @param config MoveBaseFlexConfig object
      */
     void reconfigure(move_base_flex::MoveBaseFlexConfig &config);
 
   protected:
 
+    //! the name of the loaded plugin
     std::string plugin_name_;
 
-    // class loader, to load the local planner plugin
+    //! class loader, to load the local planner plugin
     pluginlib::ClassLoader<LOCAL_PLANNER_BASE> class_loader_local_planner_;
 
-    // the local planer to calculate the velocity command
+    //! the local planer to calculate the velocity command
     boost::shared_ptr<LOCAL_PLANNER_BASE> local_planner_;
 
+    //! shared pointer to the shared tf listener
     const boost::shared_ptr<tf::TransformListener> &tf_listener_ptr;
 
+    /**
+     * @brief Sets the velocity command, to make it available for another thread
+     * @param vel_cmd_stamped current velocity command
+     */
     void setVelocityCmd(const geometry_msgs::TwistStamped &vel_cmd_stamped);
 
+    /**
+     * @brief Sets the plugin code and the plugin msg. This method is for thread communication
+     * @param plugin_code
+     * @param plugin_msg
+     */
     void setPluginInfo(const uint8_t& plugin_code, const std::string& plugin_msg);
 
-    // main thread function -> moving the robot
+    /**
+     * @brief The main run method, a thread will execute this method. It contains the main controller loop.
+     */
     virtual void run();
 
+    //! The current cycle start time of the last cycle run. Will by updated each cycle.
     ros::Time cycle_start_time;
 
+    //! The maximum number of retries
     int max_retries_;
 
+    //! The time / duration of patience, before changing the state.
     ros::Duration patience_;
 
   private:
 
+    /**
+     * @brief Pure virtual method, the derived class need to implement. Depending on the plugin base class,
+     *        some plugins need to be initialized!
+     */
     virtual void initMovingPlugin() = 0;
 
-    // stops the robot
+    /**
+     * publishes a velocity command with zero values to stop the robot.
+     */
     void publishZeroVelocity();
 
-    boost::mutex state_mtx_;
-
+    /**
+     * @brief Sets the controller state. This method makes the communication of the state thread safe.
+     * @param state The current controller state.
+     */
     void setState(ControllerState state);
 
+
+    //! mutex to handle safe thread communication for the current value of the state
+    boost::mutex state_mtx_;
+
+    //! mutex to handle safe thread communication for the current plan
     boost::mutex plan_mtx_;
+
+    //! mutex to handle safe thread communication for the current velocity command
     boost::mutex vel_cmd_mtx_;
+
+    //! mutex to handle safe thread communication for the
     boost::mutex lct_mtx_;
+
+    //! mutex to handle safe thread communication for the current plugin code
     boost::mutex pcode_mtx_;
 
+    //! true, if a new plan is available. See hasNewPlan()!
     bool new_plan_;
 
+    /**
+     * @brief Returns true if a new plan is available, false otherwise! A new plan is set by another thread!
+     * @return true, if a new plan has been set, false otherwise.
+     */
     bool hasNewPlan();
 
+    /**
+     * @brief Gets the new available plan. This method is thread safe.
+     * @param plan A reference to a plan which will then be filled with the new plan
+     */
     void getNewPlan(std::vector<geometry_msgs::PoseStamped> &plan);
 
+    //! the last calculated velocity command
     geometry_msgs::TwistStamped vel_cmd_stamped_;
 
-    // current global path
+    //! the last set plan which is currently processed by the controller
     std::vector<geometry_msgs::PoseStamped> plan_;
 
-    // condition to wake up control thread
+    //! condition variable to wake up control thread
     boost::condition_variable &condition_;
 
-    // thread for moving
+    //! the controlling thread object
     boost::thread thread_;
 
-    // timing of the moving thread
+    //! the duration which corresponds with the controller frequency.
     boost::chrono::microseconds calling_duration_;
 
-    // frames to get the current global robot pose
+    //! the frame of the robot, which will be used to determine its position.
     std::string robot_frame_;
+
+    //! the global frame the robot is controlling in.
     std::string global_frame_;
 
-    // subscribers and publishers
+    //! publisher for the current velocity command
     ros::Publisher vel_pub_;
 
-    // internal state
+    //! the current controller state
     AbstractControllerExecution::ControllerState state_;
 
-    // plugin feedback info
+    //! the last received plugin code
     uint8_t plugin_code_;
+
+    //! the last received plugin message
     std::string plugin_msg_;
 
+    //! time before a timeout used for tf requests
     double tf_timeout_;
 
-    // dynamic reconfigure attributes and methods
+    //! dynamic reconfigure config mutex, thread safe param reading and writing
     boost::recursive_mutex configuration_mutex_;
 
-    // is the moving thread running
+    //! main controller loop variable, true if the controller is running, false otherwise
     bool moving_;
   };
 
