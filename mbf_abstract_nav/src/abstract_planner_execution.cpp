f397730d5d93f2f05c54ac611aec1fa1c92fe4e3
alireza <alireza.mixedreality@gmail.com>
2018-04-27 10:08:32 +0200

    fix: Remove `xmlrpcpp` prefix  - since ROS kinetic, The `XmlRpcException.h` has been moved to `xmlrpcpp` directory,    while in the indigo distro it is located in the $ROS_DISTRO/include.  - So the include <xmlrpcpp/XmlRpcException.h> wouldn't work on the indigo distro.  - This commit provides the backward compatibility by removing the xmlrpcpp prefix    so that it can be compiled on both of kinetic and indigo distros.

--- mbf_abstract_nav/src/abstract_planner_execution.cpp
+++ mbf_abstract_nav/src/abstract_planner_execution.cpp
@@ -39,7 +39,7 @@
  */
 
 #include "mbf_abstract_nav/abstract_planner_execution.h"
-#include <xmlrpcpp/XmlRpcException.h>
+#include <XmlRpcException.h>
 #include <boost/exception/diagnostic_information.hpp>
 
 namespace mbf_abstract_nav
