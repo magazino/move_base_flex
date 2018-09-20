dca6005978873b9514f8f719aec4410c4f3c7eea
Jannik Abbenseth <jannik.abbenseth@ipa.fraunhofer.de>
2018-09-20 16:21:34 +0200

    indigo compatible nav_core interface

--- mbf_costmap_nav/src/nav_core_wrapper/wrapper_global_planner.cpp
+++ mbf_costmap_nav/src/nav_core_wrapper/wrapper_global_planner.cpp
@@ -50,7 +50,12 @@ uint32_t WrapperGlobalPlanner::makePlan(const geometry_msgs::PoseStamped &start,
                                         double &cost,
                                         std::string &message)
 {
+#ifdef USE_OLD_NAV_CORE
+  bool success = nav_core_plugin_->makePlan(start, goal, plan);
+  cost = 0;
+#else
   bool success = nav_core_plugin_->makePlan(start, goal, plan, cost);
+#endif
   message = success ? "Plan found" : "Planner failed";
   return success ? 0 : 50;  // SUCCESS | FAILURE
 }
