aadf558c37db97499a0a4f01c9fd464349003ee1
Dimitri <dnikitop@gmail.com>
2019-04-09 16:47:54 -0400

    feat: we are indigi

--- mbf_costmap_nav/src/nav_core_wrapper/wrapper_global_planner.cpp
+++ mbf_costmap_nav/src/nav_core_wrapper/wrapper_global_planner.cpp
@@ -50,7 +50,7 @@ uint32_t WrapperGlobalPlanner::makePlan(const geometry_msgs::PoseStamped &start,
                                         double &cost,
                                         std::string &message)
 {
-#if ROS_VERSION_MINIMUM(1, 12, 0) // if current ros version is >= 1.12.0
+#if ROS_VERSION_MINIMUM(1, 13, 0) // if current ros version is >= 1.12.0
   // Kinetic and beyond
   bool success = nav_core_plugin_->makePlan(start, goal, plan, cost);
 #else
