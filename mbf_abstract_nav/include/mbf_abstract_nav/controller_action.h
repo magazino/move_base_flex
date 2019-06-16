d2df4451bf8af8a46bcf720bba88b929f2e98745
Sebastian Pütz <spuetz@uos.de>
2019-06-16 15:50:22 +0200

    Concurrency container refactoring (#138)
    * Solves and closes #137 and #94 
    * Prevent LOST goals when replanning
    * New concurrency approach with containers, 
    * Set as canceled, when goals are preempted by a new plan
    * move setAccepted to abstract action

--- mbf_abstract_nav/include/mbf_abstract_nav/controller_action.h
+++ mbf_abstract_nav/include/mbf_abstract_nav/controller_action.h
@@ -59,8 +59,14 @@ class ControllerAction :
   ControllerAction(const std::string &name,
                    const RobotInformation &robot_info);
 
+  /**
+   * @brief Start controller action.
+   * Override abstract action version to allow updating current plan without stopping execution.
+   * @param goal_handle Reference to the goal handle received on action execution callback.
+   * @param execution_ptr Pointer to the execution descriptor.
+   */
   void start(
-      GoalHandle goal_handle,
+      GoalHandle &goal_handle,
       typename AbstractControllerExecution::Ptr execution_ptr
   );
 
@@ -69,24 +75,22 @@ class ControllerAction :
 protected:
   void publishExePathFeedback(
           GoalHandle& goal_handle,
-          const geometry_msgs::PoseStamped& robot_pose,
-          const geometry_msgs::PoseStamped& goal_pose,
           uint32_t outcome, const std::string &message,
           const geometry_msgs::TwistStamped& current_twist);
 
   /**
    * @brief Utility method to fill the ExePath action result in a single line
-   * @param robot_pose Current pose of the robot
-   * @param goal_pose Robot's goal pose
    * @param outcome ExePath action outcome
    * @param message ExePath action message
    * @param result The action result to fill
    */
   void fillExePathResult(
-        const geometry_msgs::PoseStamped& robot_pose,
-        const geometry_msgs::PoseStamped& goal_pose,
         uint32_t outcome, const std::string &message,
         mbf_msgs::ExePathResult &result);
+
+  boost::mutex goal_mtx_; ///< lock goal handle for updating it while running
+  geometry_msgs::PoseStamped robot_pose_; ///< Current robot pose
+  geometry_msgs::PoseStamped goal_pose_;  ///< Current goal pose
 
 };
 }
