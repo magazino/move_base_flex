48e9356bad7c199f274cac6be208fcb7d09f717f
ChristofDubs <christof.dubs@gmx.ch>
2020-01-15 13:28:43 +0100

    Add parameter to actively stop once the goal is reached (#166)

--- mbf_abstract_nav/include/mbf_abstract_nav/abstract_controller_execution.h
+++ mbf_abstract_nav/include/mbf_abstract_nav/abstract_controller_execution.h
@@ -332,6 +332,9 @@ namespace mbf_abstract_nav
     //! whether move base flex should check for the goal tolerance or not.
     bool mbf_tolerance_check_;
 
+    //! whether move base flex should force the robot to stop once the goal is reached.
+    bool force_stop_at_goal_;
+
     //! distance tolerance to the given goal pose
     double dist_tolerance_;
 
