15d05fa24a6a52dce3e7c5dfc2498845993fe491
Renan Salles <renan028@gmail.com>
2023-03-29 11:15:43 +0900

    bag only related files

--- mbf_costmap_nav/test/record_bag_file.sh
+++ mbf_costmap_nav/test/record_bag_file.sh
@@ -5,4 +5,6 @@
 OUTPUT_PATH="$(echo $1 | sed 's![^/]$!&/!')"
 echo "Bag file will be saved in "$OUTPUT_PATH
 
-rosbag record -o "$OUTPUT_PATH"mbf_test -e "(.*)"
+rosbag record -o "$OUTPUT_PATH"mbf_test \
+              -e "(.*)/search_markers(.*)" \
+              -e "(.*)/map(.*)" \
